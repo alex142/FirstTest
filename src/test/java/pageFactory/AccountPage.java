cab7a99d72b09939315494f24bb7675db8ea848f
Oleksii Kukharenko <oleksii.kukharenko142@gmail.com>
2017-12-03 23:43:53 +0200

    PageObjects HW

---
+++ src/test/java/pageFactory/AccountPage.java
@@ -0,0 +1,19 @@
+package pageFactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AccountPage {
+    private WebDriver webDriver;
+
+    @FindBy(xpath = "//*[@id=\"header\"]/div[2]/div/div/nav/div[2]/a")
+    private WebElement logoutButton;
+
+    protected LoginPage signOut(){
+        logoutButton.click();
+        return new LoginPage(webDriver);
+    }
+
+    public AccountPage(WebDriver webDriver){this.webDriver = webDriver;}
+}
